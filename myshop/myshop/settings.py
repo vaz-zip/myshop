bab9f449f488f263142e7cc5998ec0e45850b3eb
vaz-zip <baldeha@gmail.com>
2023-05-30 14:39:48 +0400

    'Create app cart'

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
+
 
 ]
 
@@ -128,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CART_SESSION_ID = 'cart'
