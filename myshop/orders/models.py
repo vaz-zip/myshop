2372b3f3e4b471d07cfee16b9814dcd7e66a47f1
vaz-zip <baldeha@gmail.com>
2023-06-03 08:21:36 +0400

    'create model Order'

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from shop.models import Product
+
+
+
+class Order(models.Model):
+    first_name = models.CharField(max_length=64, verbose_name='Имя')
+    last_name = models.CharField(max_length=64, verbose_name='Фамилия')
+    email = models.EmailField(verbose_name='Email')
+    address = models.CharField(max_length=64, verbose_name='Адрес')
+    postal_code = models.CharField(max_length=16, verbose_name='Индекс')
+    city = models.CharField(max_length=32, validators='Регион')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Создан')
+    updated = models.DateTimeField(auto_now_add=True, verbose_name='Скорректирован')
+    paid = models.BooleanField(default=False, verbose_name='Оплачен')
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return f'Order {self.id}'
+
+    def get_total_cost(self):
+        return sum(item.get_cost() for item in self.items.all())   
 
 # Create your models here.
